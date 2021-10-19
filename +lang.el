e258f9d8a119ed1bbd976a15701727292799168e
dej <dej1314@163.com>
2021-10-19 18:10:12 +0800

    use goimports

--- +lang.el
+++ +lang.el
@@ -1,4 +1,10 @@
 ;;; +lang.el -*- lexical-binding: t; -*-
 
 ;; golang
+(setq-hook! 'go-mode-hook +format-with-lsp nil)
+
+;; or
+
+;; (setq gofmt-command "goimports")
+;; (add-hook 'before-save-hook 'gofmt-before-save)
 
--- init.el
+++ init.el
@@ -196,3 +196,4 @@
        :private
        autosave
        table)
+
